87d79722d1bfadf486f136e562ea22f50c21a9ed
Matheus <mps.web.tech@gmail.com>
2026-03-10 06:19:52 -0300

    feat: create route and files

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,17 @@
 <?php
 
+//Libs
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+// Routes
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return response()->json(["message" => "API BeTalent Ativa"]);
 });
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::post('/login', [AuthController::class, 'login']);
 
+Route::middleware('auth:sanctum')->group(function () {
+    
 });
